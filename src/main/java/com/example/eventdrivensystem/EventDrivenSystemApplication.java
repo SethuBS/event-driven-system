fd9998ef8c9dd4c73cf895e7cd3f13058f534b39
Sethu <sethuserge@gmail.com>
2024-09-11 10:53:43 +0200

    Initial commit.

---
+++ src/main/java/com/example/eventdrivensystem/EventDrivenSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.eventdrivensystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventDrivenSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventDrivenSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/eventdrivensystem/EventDrivenSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eventdrivensystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventDrivenSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
